b846467835174aebe1e44e44e47bb1886635812c
Alexey Kuznetsov <m2lxkuz@gmail.com>
2020-03-08 03:06:01 +0500

    Improve articles API
    Now it supports groups, stories, ordering and articles filtering

--- app/controllers/api/articles_controller.rb
+++ app/controllers/api/articles_controller.rb
@@ -1,15 +1,45 @@
+# frozen_string_literal: true
+
 class Api::ArticlesController < ApplicationController
   def index
-    articles =  ArticlesSearcher.new(params).call
-    articles_data = present_articles(articles)
-    render json: articles_data
+    search_params = permit_params(params)
+    searcher = build_searcher(search_params)
+    search_results = searcher.new(search_params).call
+    presenter = build_presenter(search_params)
+    response_data = present_list(search_results, presenter)
+    render json: response_data
   end
 
   private
 
-  def present_articles(articles)
-    articles.map do |article|
-      ArticlePresenter.new(article).call
+  def build_searcher(search_params)
+    if search_params[:stories] && !search_params[:group]
+      StoriesSearcher
+    else
+      ArticlesSearcher
     end
+  end
+
+  def build_presenter(search_params)
+    if search_params[:group]
+      ArticlesGroupPresenter
+    elsif search_params[:stories]
+      StoryPresenter
+    else
+      ArticlePresenter
+    end
+  end
+
+  def present_list(list, presenter)
+    list.map do |item|
+      presenter.new(item).call
+    end
+  end
+
+  def permit_params(params)
+    keys = [:stories] +
+           ArticlesSearcher::SEARCH_PARAMS +
+           StoriesSearcher::SEARCH_PARAMS
+    params.permit(*keys)
   end
 end
