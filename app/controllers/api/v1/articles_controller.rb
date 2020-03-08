c38f4516ab5b48842f0c0413dfba1c97b570a40a
Alexey Kuznetsov <m2lxkuz@gmail.com>
2020-03-08 13:26:36 +0500

    Wrap Articles API controller with version namespace

---
+++ app/controllers/api/v1/articles_controller.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class ArticlesController < ApplicationController
+      def index
+        search_params = permit_params(params)
+        searcher = build_searcher(search_params)
+        search_results = searcher.new(search_params).call
+        presenter = build_presenter(search_params)
+        response_data = present_list(search_results, presenter)
+        render json: response_data
+      end
+
+      private
+
+      def build_searcher(search_params)
+        if search_params[:stories] && !search_params[:group]
+          StoriesSearcher
+        else
+          ArticlesSearcher
+        end
+      end
+
+      def build_presenter(search_params)
+        if search_params[:group]
+          ArticlesGroupPresenter
+        elsif search_params[:stories]
+          StoryPresenter
+        else
+          ArticlePresenter
+        end
+      end
+
+      def present_list(list, presenter)
+        list.map do |item|
+          presenter.new(item).call
+        end
+      end
+
+      def permit_params(params)
+        keys = [:stories] +
+              ArticlesSearcher::SEARCH_PARAMS +
+              StoriesSearcher::SEARCH_PARAMS
+        params.permit(*keys)
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
-    resources :articles, only: :index
+    namespace :v1 do
+      resources :articles, only: :index
+    end
   end
 end
