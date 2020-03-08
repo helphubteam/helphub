15501f5078d262a3e5f4a8d1a98d5320df9a3fa9
Alexey Kuznetsov <m2lxkuz@gmail.com>
2020-03-08 12:43:36 +0500

    Setup different kinds for articles seeds

--- db/seeds/articles.rb
+++ db/seeds/articles.rb
@@ -5,7 +5,8 @@ unless Article.any?
   CSV.foreach('./db/seeds/articles.csv', headers: true) do |row|
     article_data << {
       name: row['title'],
-      content: row['text']
+      content: row['text'],
+      kind: rand(0..Article.kinds.values.max)
     }
   end
   Article.import(article_data)
