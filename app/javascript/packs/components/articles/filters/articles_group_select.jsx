b6d1a7bc77382f298e77a34c5b6556c568a31f1f
Alexey Kuznetsov <m2lxkuz@gmail.com>
2020-03-11 01:58:05 +0500

    Add articles group select component

---
+++ app/javascript/packs/components/articles/filters/articles_group_select.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { observer } from 'mobx-react'
+import articlesStore, { GROUP_OPTIONS } from '../../../stores/articles_store';
+
+const ArticlesGroupSelect = observer(() => (
+  <select
+    name='group'
+    value={articlesStore.filters.group}
+    className="form-control"
+    onChange={ ev => articlesStore.setFilter('group', ev.target.value) }
+    placeholder="Group results by">
+    <option></option>
+    {
+      GROUP_OPTIONS.map(option => <option key={option} value={option}>{option}</option>)
+    }
+  </select>
+))
+
+export default ArticlesGroupSelect
