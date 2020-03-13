62cc390dcf0344e8c2d83cce802da5a987cd6e27
Alexey Kuznetsov <m2lxkuz@gmail.com>
2020-03-13 05:44:12 +0500

    Remove useless method from notifications channel

--- app/channels/notifications_channel.rb
+++ app/channels/notifications_channel.rb
@@ -6,11 +6,4 @@ class NotificationsChannel < ApplicationCable::Channel
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
   end
-
-  def speak(data)
-    ActionCable.server.broadcast(
-      "NotificationsChannel",
-      message: data[data]
-    )
-  end
 end
