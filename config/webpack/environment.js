11237e80e983f1a0092aff9b3a62c478e8701b7c
Alexey Kuznetsov <m2lxkuz@gmail.com>
2020-03-13 16:12:17 +0500

    Action Cable package fix for babel
    https://github.com/rails/rails/issues/35501

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,11 @@
 const { environment } = require('@rails/webpacker')
 
+const nodeModulesLoader = environment.loaders.get('nodeModules')
+if (!Array.isArray(nodeModulesLoader.exclude)) {
+  nodeModulesLoader.exclude = (nodeModulesLoader.exclude == null)
+    ? []
+    : [nodeModulesLoader.exclude]
+}
+nodeModulesLoader.exclude.push(/actioncable/)
+
 module.exports = environment
