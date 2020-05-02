08a6f1c462ccbe1c351e08404d243c9fe1999d4b
Alexey Kuznetsov <m2lxkuz@gmail.com>
2020-05-02 21:36:44 +0500

    Cleanup blogpost models

--- app/models/story.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class Story < ApplicationRecord
-  validates :name, presence: true, uniqueness: true
-
-  has_and_belongs_to_many :articles
-
-  include LiveUpdatable
-end
