7fc8d8415e5866b7892bb613fc5e981e3de852a1
Alexey Kuznetsov <m2lxkuz@gmail.com>
2020-05-03 21:52:39 +0500

    Polish docker infrastructure
    Make it safe for entering russian symbols

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
 rm -f tmp/pids/server.pid
+RUBYOPT='-W0' bundle install
 RUBYOPT='-W0' bundle exec rake db:create db:migrate db:seed
 RUBYOPT='-W0' bundle exec rails s -p 3000 -b 0.0.0.0
