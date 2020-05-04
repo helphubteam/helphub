93221632f122753ef86813ff148f9c56afb731a6
Alexey Kuznetsov <m2lxkuz@gmail.com>
2020-05-04 16:30:40 +0500

    Add wait for postgres script to docker

--- start.sh
+++ start.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 set -e
+until psql -h postgres -U postgres -c '\l'; do
+  echo "Postgres is unavailable - sleeping for 5 seconds"
+  sleep 5
+done
+echo "Postgres is up - executing command"
 rm -f tmp/pids/server.pid
 RUBYOPT='-W0' bundle install
 RUBYOPT='-W0' bundle exec rake db:create db:migrate db:seed
