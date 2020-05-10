576f605434f87df70595732187756ae7187035fb
Alexey Kuznetsov <m2lxkuz@gmail.com>
2020-05-10 18:21:36 +0500

    Add seeds for help requests

---
+++ db/seeds/help_requests.rb
@@ -0,0 +1,16 @@
+unless HelpRequest.any?
+  points = [
+    ['37.66868591308594','55.71164005362048'],
+    ['37.71125793457031','55.79433344350657' ],
+    ['37.54852294921875','55.77502825125135']
+  ]
+
+  points.each_with_index do |coordinates, index|
+    HelpRequest.create!(
+      lonlat_geojson: { type: 'Point', coordinates: coordinates }.to_json,
+      address: "Moscow, Lenina street, #{index + 1}",
+      person: 'Some Test Person',
+      phone: '+79221111111'
+    )
+  end
+end
