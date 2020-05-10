e5c558288f51d6fd42f5d38df67528df4fd9d680
iL53n <ilsegribko@gmail.com>
2020-05-10 22:23:02 +0300

    move authorization to api::v1::authorization_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,24 +1,5 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  def authorize_request
-    @decoded = JsonWebToken.decode(http_auth_header)
-    @current_user = User.find(@decoded[:user_id])
-  rescue ActiveRecord::RecordNotFound => e
-    render_error_message(e, :unauthorized)
-  rescue JWT::DecodeError => e
-    render_error_message(e, :unauthorized)
-  end
 
-  private
-
-  def http_auth_header
-    header = request.headers['Authorization']
-    header&.split(' ')&.last
-  end
-
-  def render_error_message(error, status)
-    render json: { errors: error.message },
-           status: status
-  end
 end
