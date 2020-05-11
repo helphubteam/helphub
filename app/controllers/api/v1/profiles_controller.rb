e40203d336cc65eedb32faac01b764edad8242e1
iL53n <ilsegribko@gmail.com>
2020-05-11 23:33:04 +0300

    create user presenter

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class ProfilesController < Api::V1::BaseController
       def show
-        render json: current_user, status: :ok
+        render json: UserPresenter.new(current_user).call, status: :ok
       end
     end
   end
