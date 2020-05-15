71d34f660a837eb8bad2145e3673d7f6875a1ffd
Alexey Kuznetsov <m2lxkuz@gmail.com>
2020-05-16 02:20:19 +0500

    Add state machine to help_requests

--- app/models/help_request.rb
+++ app/models/help_request.rb
@@ -1,6 +1,35 @@
 class HelpRequest < ApplicationRecord
   include GeojsonAccessor
-  
+
   geojson_accessor :lonlat
 
+  belongs_to :volunteer, class_name: 'User', optional: true
+
+  enum state: {active: 0, assigned: 1, submitted: 2, blocked: 3} do
+    event :assign do
+      transition :active => :assigned
+    end
+
+    event :submit do
+      transition :assigned => :submitted
+    end
+
+    event :refuse do
+      before do
+        self.volunteer = nil
+      end
+
+      transition :assigned => :active
+    end
+
+    event :activate do
+      transition :blocked => :active
+    end
+
+    event :block do
+      transition all - [:blocked] => :blocked
+    end
+  end
+
+  scope :active, -> { where(state: :active) }
 end
