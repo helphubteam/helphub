377a56746314daa942cceb248051a67b9cf01185
iL53n <ilsegribko@gmail.com>
2020-05-16 00:12:32 +0300

    config devise mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,8 +62,17 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  # ToDo: need proper SMTP configuration
   config.action_mailer.default_url_options = { host: "http://covihelp.herokuapp.com/" }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      domain:               'gmail.com',
+      port:                 587,
+      user_name:            'moviescollectionIWMteam@gmail.com',  # need ENV variable -- ENV['SMTP_USERNAME']
+      password:             'moviesqwerty',                       # need ENV variable -- ENV['SMTP_PASSWORD']
+      authentication:       'plain',
+      enable_starttls_auto: true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
