96751c59810a4218b9455e28df94504bd92d98a2
Valeriy <sten.vl2010@gmail.com>
2020-05-16 14:19:31 +0500

    Add simple map component

---
+++ app/javascript/packs/map.js
@@ -0,0 +1,26 @@
+/* eslint-disable global-require */
+/* eslint-disable no-new */
+import Vue from 'vue/dist/vue.esm.js';
+import LeafletMap from '../components/LeafletMap'
+import 'leaflet/dist/leaflet.css';
+import 'leaflet-draw/dist/leaflet.draw.css'
+
+// Known issue with missing marker icons. See: https://vue2-leaflet.netlify.app/quickstart/#marker-icons-are-missing.
+// eslint-disable-next-line
+delete L.Icon.Default.prototype._getIconUrl;
+
+// eslint-disable-next-line
+L.Icon.Default.mergeOptions({  
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),  
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),  
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png')  
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+  new Vue({
+    el: '#vue-app',
+    components: {
+      LeafletMap
+    }
+  });
+});
