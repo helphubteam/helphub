383e770866a66bb169a0167df4699a5eb3cac219
Alexey Kuznetsov <m2lxkuz@gmail.com>
2020-05-19 01:44:07 +0500

    Fix too many redirects issue

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 
-  devise_for :users
+  devise_for :users, controllers: { sessions: "sessions" }
 
   namespace :api do
     namespace :v1 do
