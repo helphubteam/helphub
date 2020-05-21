6504ff827a5fe4111843809602439bb126913393
Valeriy <sten.vl2010@gmail.com>
2020-05-22 00:32:24 +0500

    Move help_request and user presenters

---
+++ app/presenters/api/user_presenter.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class UserPresenter < BasePresenter
+    def call
+      super.merge({
+                    role: target.role,
+                    email: target.email,
+                    created_at: target.created_at.to_i,
+                    updated_at: target.updated_at.try(:to_i)
+                  })
+    end
+  end
+  
