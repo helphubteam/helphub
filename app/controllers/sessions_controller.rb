eff19b55ea19dff0ac0630625491e8adc7431e0e
iL53n <ilsegribko@gmail.com>
2020-05-23 01:44:31 +0300

    add moderator in session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,12 +2,11 @@ class SessionsController < Devise::SessionsController
 
   def create
     super do
-      if !resource.admin?
+      unless resource.admin? || resource.moderator?
         signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
         set_flash_message :notice, :signed_out if signed_out && is_flashing_format?
         respond_to_on_destroy
       end
     end
   end
-
 end
