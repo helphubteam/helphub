6fdccd92a8691c6e2bb9e36046f620653733cb6e
Alexey Kuznetsov <m2lxkuz@gmail.com>
2020-05-23 02:27:05 +0500

    Redirect to helphub.ru in case volunteer registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,13 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  
+  LADING_WEBSITE_URL = "http://helphub.ru"  
+
   protected
   
   def after_sign_in_path_for(resource)
-    if resource.admin?
-      return admin_help_requests_path
-    else
-      # ToDo: render mobile link after attempt volonteer sign_in
-    end
-    super
+    return admin_help_requests_path if resource.admin?
+
+    LADING_WEBSITE_URL
   end
 end
