c85bf250b485aaa8a645f816376111d76206cc19
Valeriy <sten.vl2010@gmail.com>
2020-05-23 11:24:49 +0500

    Apply rubocop autofix

--- app/presenters/help_request_presenter.rb
+++ app/presenters/help_request_presenter.rb
@@ -1,26 +1,25 @@
 # frozen_string_literal: true
 
 class HelpRequestPresenter
-    delegate :id,
-      :phone,
-      :address,
-      :state,
-      :comment,
-      :person,
-      :mediated,
-      :meds_preciption_required,
-      :volunteer_id, to: :target
-  
-    def initialize(target)
-      @target = target
-    end
-  
-    def address
-        [target.city, target.district, target.street, target.house, target.apartment].compact.join(' ')
-    end
-  
+  delegate :id,
+           :phone,
+           :address,
+           :state,
+           :comment,
+           :person,
+           :mediated,
+           :meds_preciption_required,
+           :volunteer_id, to: :target
+
+  def initialize(target)
+    @target = target
+  end
+
+  def address
+    [target.city, target.district, target.street, target.house, target.apartment].compact.join(' ')
+  end
+
     private
-  
-    attr_reader :target
-  
+
+  attr_reader :target
   end
