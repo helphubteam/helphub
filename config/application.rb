650e933d9c6fdc3044758682d8d410d2d0e7f848
Alexey Kuznetsov <m2lxkuz@gmail.com>
2020-05-26 02:03:25 +0500

    Add i18n

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module App
     config.load_defaults 6.0
     config.autoload_paths << "#{Rails.root}/lib"
     config.i18n.default_locale = :ru
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
