7d1531ec64ac5ce5efb3a0ed6e8736a5a185ae9d
Alexey Kuznetsov <m2lxkuz@gmail.com>
2020-05-29 01:58:01 +0500

    Add privacy level into help request presenter

--- app/presenters/api/help_request_presenter.rb
+++ app/presenters/api/help_request_presenter.rb
@@ -2,7 +2,7 @@
 
 module Api
   class HelpRequestPresenter
-    AVAILABLE_ATTRIBUTES = %i[
+    FULL_ATTRIBUTES = %i[
       id
       phone
       state
@@ -14,39 +14,65 @@ module Api
       volunteer_id
     ].freeze
 
+    NON_PERSONAL_ATTRIBUTES = %i[
+      id
+      state
+      comment
+      number
+      mediated
+      meds_preciption_required
+      volunteer_id
+    ]
+
     def initialize(target, current_user)
       @target = target
       @current_user = current_user
     end
 
     def call
+      return full_data if target.volunteer == current_user and target.assigned?
+
+      non_personal_data
+    end
+
+    private
+
+    def non_personal_data
       target
         .attributes
-        .slice(*AVAILABLE_ATTRIBUTES.map(&:to_s))
+        .slice(*NON_PERSONAL_ATTRIBUTES.map(&:to_s))
         .merge(
-          address: address,
-          lonlat: lonlat_geojson,
-          geo_salt: geo_salt?,
+          address: non_personal_address,
+          lonlat: JSON.parse(target.lonlat_with_salt_geojson),
+          geo_salt: true,
           created_at: target.created_at.to_i,
           updated_at: target.updated_at.try(:to_i)
         )
     end
 
-    private
+    def full_data
+      target
+        .attributes
+        .slice(*FULL_ATTRIBUTES.map(&:to_s))
+        .merge(
+          address: full_address,
+          lonlat: JSON.parse(target.lonlat_geojson),
+          geo_salt: false,
+          created_at: target.created_at.to_i,
+          updated_at: target.updated_at.try(:to_i)
+        )
+    end
 
     attr_reader :target, :current_user
 
-    def geo_salt?
-      target.active? || target.volunteer != current_user
-    end
-
-    def lonlat_geojson
-      lonlat = geo_salt? ? target.lonlat_with_salt_geojson : target.lonlat_geojson
-      JSON.parse(lonlat)
-    end
-
-    def address
+    def full_address
       [target.city, target.district, target.street, target.house, target.apartment].compact.join(' ')
+    end
+
+    def non_personal_address
+      non_personal_address = [ target.city, target.district, target.street]
+      non_personal_address.push(target.house) if target.apartment.present?
+      non_personal_address.compact.join(' ')      
     end
   end
 end
