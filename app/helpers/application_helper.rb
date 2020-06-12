a6f970173258453f1b359dfbad74add130252028
iL53n <ilsegribko@gmail.com>
2020-06-12 11:00:27 +0300

    add sort_link helper for tables

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -27,4 +27,13 @@ module ApplicationHelper
       datetime.to_s
     end
   end
+
+  def sort_link(column, title = nil)
+    title ||= column.titleize
+    direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
+
+    icon = sort_direction == "asc" ? "glyphicon glyphicon-chevron-down" : "glyphicon glyphicon-chevron-up"
+    icon = column == sort_column ? icon : ""
+    link_to "#{title} <span class='#{icon}'></span>".html_safe, {column: column, direction: direction}
+  end
 end
