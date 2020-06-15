f652749b4c3dc0887e2c28602f610cbb02c0334b
iL53n <ilsegribko@gmail.com>
2020-06-15 21:35:41 +0300

    fix/volonteer login -  double render

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,11 @@
 class SessionsController < Devise::SessionsController
-  prepend_before_action :require_no_authentication, :only => [ :new, :create ]
+  prepend_before_action :require_no_authentication, only: [:new, :create]
 
   def create
     super do
       unless resource.admin? || resource.moderator?
         signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
         set_flash_message :notice, :signed_out if signed_out && is_flashing_format?
-        respond_to_on_destroy
       end
     end
   end
