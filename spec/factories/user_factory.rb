038be1e767b7f09b022dc537509859afed502c4e
Alexey Kuznetsov <m2lxkuz@gmail.com>
2020-06-24 23:09:51 +0500

    Add API login request spec

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -3,8 +3,13 @@ FactoryBot.define do
   factory :user do
     email { "test@email.local" }
     password { '123qwe123qwe!' }
+    
     trait :admin do
       role { 'admin' }
+    end
+    
+    trait :volunteer do
+      role { 'volunteer' }
     end
   end
 end
