672e655cc98ab8b1f5e76e6aa9d382c164b8d078
Alexey Kuznetsov <m2lxkuz@gmail.com>
2020-06-29 23:12:43 +0500

    Cover help request Admin and API with simple tests

---
+++ spec/factories/help_request_factory.rb
@@ -0,0 +1,26 @@
+# This will guess the User class
+FactoryBot.define do
+  factory :help_request do
+    lonlat_geojson { "{\"type\":\"Point\",\"coordinates\":[37.54852294921875,55.77502825125135]}" }
+    phone { '123123123' }
+    city { 'Moscow' }
+    district { 'Center' }
+    street { 'Test Street' }
+    house { '12' }
+    apartment { '1' }
+    comment { 'test comment' }
+    person { 'test person' }
+    state { 'active' }
+    organization
+
+    trait :assigned do
+      state { 'assigned' }
+      volunteer { create :user, :volunteer }
+    end
+
+    trait :submitted do
+      state { 'submitted' }
+      volunteer { create :user, :volunteer }
+    end
+  end
+end
---
+++ spec/requests/api/v1/help_requests_spec.rb
@@ -0,0 +1,52 @@
+require 'rails_helper'
+
+RSpec.describe 'Api::V1::HelpRequests', type: :request do
+  include_context 'jwt authenticated'
+
+  let(:user) { create :user, :volunteer, organization: organization }
+  let(:organization) { create :organization }
+ 
+  describe 'GET /api/v1/help_requests' do
+    it 'returns empty array' do
+      get api_v1_help_requests_path
+      expect(response).to have_http_status(200)
+      expect(JSON.parse(response.body)).to eq([])
+    end
+  end
+
+  describe 'POST /api/v1/help_requests/:id/assign' do
+    let!(:help_request) { create :help_request, organization: organization }
+
+    it 'assigns HelpRequest record' do
+      expect(help_request.volunteer).to be_nil
+      expect(help_request.state).to eq('active')
+      post(assign_api_v1_help_request_path(help_request))
+      expect(help_request.reload.volunteer).to eq(user)
+      expect(help_request.state).to eq('assigned')
+    end
+  end
+
+  describe 'POST /api/v1/help_requests/:id/refuse' do
+    let!(:help_request) { create :help_request, :assigned, volunteer: user, organization: organization }
+
+    it 'refuses HelpRequest record' do
+      expect(help_request.volunteer).to eq(user)
+      expect(help_request.state).to eq('assigned')
+      post(refuse_api_v1_help_request_path(help_request))
+      expect(help_request.reload.volunteer).to be_nil
+      expect(help_request.state).to eq('active')
+    end
+  end
+
+  describe 'POST /api/v1/help_requests/:id/submit' do
+    let!(:help_request) { create :help_request, :assigned, volunteer: user, organization: organization }
+
+    it 'refuses HelpRequest record' do
+      expect(help_request.volunteer).to eq(user)
+      expect(help_request.state).to eq('assigned')
+      post(submit_api_v1_help_request_path(help_request))
+      expect(help_request.reload.volunteer).to eq(user)
+      expect(help_request.state).to eq('submitted')
+    end
+  end
+end
