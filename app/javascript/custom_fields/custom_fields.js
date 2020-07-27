12bc44d03e9d4881cd52cfc664afe3bd91d5a81a
Alexey Kuznetsov <m2lxkuz@gmail.com>
2020-07-28 02:36:58 +0500

    Polish custom_fields js component

--- app/javascript/custom_fields/custom_fields.js
+++ app/javascript/custom_fields/custom_fields.js
@@ -17,35 +17,47 @@ export default class CustomFieldsPanel {
 
   renderData(data) {
 
+    function fieldName(index, key) {
+      return `help_request[custom_values_attributes][${index}][${key}]`
+    }
+
+    function fieldId(index) {
+      return `help_request_custom_values_attributes_${index}_value`
+    }
+
+    function renderHiddenFields(obj, index) {
+      return `<input
+        type="hidden"
+        value="${obj.custom_field_id}"
+        name="${fieldName(index, 'custom_field_id')}"
+      />${ obj.id && `<input
+        type="hidden"
+        value="${obj.id}"
+        name="${fieldName(index, 'id')}"
+      />` || ''}`
+    }
+
     function renderText(obj, index) {
       return `<div class="form-group row help_request_custom_values_value">
         <label
           class="col-sm-3 col-form-label string optional"
-          :for="help_request_custom_values_attributes_${index}_value" 
+          for="${fieldId(index)}" 
         >
           ${obj.name}
         </label>
         <div class="col-sm-9">
-          <input id="x" type="hidden" value="${obj.value}" name="content">
+          <input id="x" type="hidden" value="${obj.value || ''}" name="${fieldName(index, 'value')}">
           <trix-editor input="x"></trix-editor>
         </div>
-      </div>
-      <input
-        type="hidden"
-        value="${obj.custom_field_id}"
-        name="help_request[custom_values_attributes][${index}][custom_field_id]"
-      />${ obj.id && `<input
-        type="hidden"
-        value="${obj.id}"
-        name="help_request[custom_values_attributes][${index}][id]"
-      />` || ''}`
+        ${renderHiddenFields(obj, index)}
+      </div>`
     }
 
     function renderString(obj, index) {
       return `<div class="form-group row string optional help_request_custom_values_value">
         <label
           class="col-sm-3 col-form-label string optional"
-          :for="help_request_custom_values_attributes_${index}_value" 
+          for="${fieldId(index)}" 
         >
           ${obj.name}
         </label>
@@ -53,134 +65,64 @@ export default class CustomFieldsPanel {
           <input
             class="form-control string optional"
             type="text"
-            name="help_request[custom_values_attributes][${index}][value]" 
-            id="help_request_custom_values_attributes_${index}_value"
-            value="${obj.value}"
+            name="${fieldName(index, 'value')}" 
+            id="${fieldId(index)}"
+            value="${obj.value || ''}"
           />
         </div>
-      </div>
-      <input
-        type="hidden"
-        value="${obj.custom_field_id}"
-        name="help_request[custom_values_attributes][${index}][custom_field_id]"
-      />${ obj.id && `<input
-        type="hidden"
-        value="${obj.id}"
-        name="help_request[custom_values_attributes][${index}][id]"
-      />` || ''}`
+        ${renderHiddenFields(obj, index)}
+      </div>`
     }
 
     function renderDate(obj, index) {
       return `<div class="form-group row string optional help_request_custom_values_value">
         <label
           class="col-sm-3 col-form-label string optional"
-          :for="help_request_custom_values_attributes_${index}_value" 
+          for="${fieldId(index)}" 
         >
           ${obj.name}
         </label>
-        <div class="col-sm-2">
+        <div class="col-sm-3">
           <input
             class="form-control string optional"
             type="date"
-            name="help_request[custom_values_attributes][${index}][value]" 
-            id="help_request_custom_values_attributes_${index}_value"
+            name="${fieldName(index, 'value')}" 
+            id="${fieldId(index)}"
             value="${obj.value}"
           />
         </div>
-      </div>
-      <input
-        type="hidden"
-        value="${obj.custom_field_id}"
-        name="help_request[custom_values_attributes][${index}][custom_field_id]"
-      />${ obj.id && `<input
-        type="hidden"
-        value="${obj.id}"
-        name="help_request[custom_values_attributes][${index}][id]"
-      />` || ''}`
+        ${renderHiddenFields(obj, index)}
+      </div>`
     }
 
     function renderCheckbox(obj, index) {
-      return `<div class="form-group row string optional help_request_custom_values_value">
-        <label
-          class="col-sm-3 col-form-label string optional"
-          :for="help_request_custom_values_attributes_${index}_value" 
-        >
-          ${obj.name}
-        </label>
-        <div class="col-sm-1">
-          <input
-            class="form-control string optional"
-            type="checkbox"
-            name="help_request[custom_values_attributes][${index}][value]" 
-            id="help_request_custom_values_attributes_${index}_value"
-            value="${obj.value}"
-          />
-        </div>
-      </div>
-      <input
-        type="hidden"
-        value="${obj.custom_field_id}"
-        name="help_request[custom_values_attributes][${index}][custom_field_id]"
-       />${ obj.id && `<input
-         type="hidden"
-         value="${obj.id}"
-         name="help_request[custom_values_attributes][${index}][id]"
-       />` || ''}`
+      return `
+        <fieldset class="form-group boolean required help_request_person">
+          <div class="form-check">
+            <input name="${fieldName(index, 'value')}" type="hidden" value="0" />
+            <input class="form-check-input boolean required" type="checkbox" value="1" ${obj.value == '1' && 'checked="checked"'} name="${fieldName(index, 'value')}" id="${fieldId(index)}" />
+            <label class="form-check-label boolean required" for="${fieldId(index)}">
+              ${obj.name}
+            </label>
+          </div>
+          ${renderHiddenFields(obj, index)}
+        </fieldset>
+      `
     }
 
     this.container.innerHTML = data.map((obj, index) => {
-      let contentArr = [];
       let dtype = obj.data_type;
-      // const checkboxValue = obj.data_type;
-      // let val = '';
-      // if (checkboxValue === 'checkbox') {
-      //   val = obj.value;
-      // }
-      // eslint-disable-next-line default-case
       switch (dtype) {
         case 'string':
-          contentArr.push(renderString(obj, index));
-          break;
+          return renderString(obj, index);
         case 'textarea':
-          contentArr.push(renderText(obj, index));
-          break;
+          return renderText(obj, index);
         case 'date':
-          contentArr.push(renderDate(obj, index));
-          break;
+          return  renderDate(obj, index);
         case 'checkbox':
-          contentArr.push(renderCheckbox(obj, index));
-          break;
+          return renderCheckbox(obj, index);
       }
-    //   return `<div class="form-group row string optional help_request_custom_values_value">
-    //     <label
-    //       class="col-sm-3 col-form-label string optional"
-    //       :for="help_request_custom_values_attributes_${index}_value"
-    //     >
-    //       ${obj.name}
-    //     </label>
-    //     <div class="col-sm-9">
-    //       <input
-    //         class="form-control string optional"
-    //         type="${obj.data_type}"
-    //         name="help_request[custom_values_attributes][${index}][value]"
-    //         id="help_request_custom_values_attributes_${index}_value"
-    //         value=val
-    //         checked
-    //       />
-    //     </div>
-    //   </div>
-    //   <input
-    //     type="hidden"
-    //     value="${obj.custom_field_id}"
-    //     name="help_request[custom_values_attributes][${index}][custom_field_id]"
-    //   />${ obj.id && `<input
-    //     type="hidden"
-    //     value="${obj.id}"
-    //     name="help_request[custom_values_attributes][${index}][id]"
-    //   />` || ''}`
-    // }).join('')
-      return contentArr.join('')
-  })
+    }).join('')
   }
 
 
