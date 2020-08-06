3d7dbf7f00a7e7d5ca490977326a2894921f82ec
Alexey Kuznetsov <m2lxkuz@gmail.com>
2020-08-07 02:49:11 +0500

    Add refresh_roken API endpoint

--- spec/requests/api/v1/login_spec.rb
+++ spec/requests/api/v1/login_spec.rb
@@ -21,8 +21,31 @@ RSpec.describe 'Api::V1::Login', type: :request do
 
       it 'responds unathorized error' do
         post api_v1_login_path, params: { email: email, password: wrong_password }
-        expect(response).to have_http_status(401)
+        expect(response).to have_http_status(403)
         expect(JSON.parse(response.body)).to eq({ 'errors' => [{ 'message' => 'Неверные логин или пароль' }] })
+      end
+    end
+  end
+
+  describe 'POST /api/v1/refresh_token' do
+    context 'when not authroized' do
+      it 'responds unathorized error' do
+        post api_v1_refresh_token_path
+        expect(response).to have_http_status(401)
+        expect(JSON.parse(response.body)).to eq({ 'errors' => [{ 'message' => 'Nil JSON web token' }] })
+      end
+    end
+
+    context 'when authorized' do
+      include_context 'jwt authenticated'
+      
+      let(:user) { create :user, :volunteer, organization: organization }
+      let(:organization) { create :organization }
+
+      it 'responds a new token' do
+        post api_v1_refresh_token_path
+        expect(response).to have_http_status(200)
+        expect(JSON.parse(response.body)).to include('token', 'expiration_date', 'email')
       end
     end
   end
