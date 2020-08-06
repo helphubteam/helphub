0a924c09f81748bbbeaf43f0245150cd75271167
RodevichDA <XwyKCcf3>
2020-08-06 18:48:22 +0300

    fix small

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,13 +1,11 @@
 class Organization < ApplicationRecord
-  has_many :users, dependent: :destroy
-  has_many :help_requests,  dependent: :destroy
+  has_many :users, dependent: :restrict_with_error
+  has_many :help_requests, dependent: :restrict_with_error
   has_many :help_request_kinds, dependent: :destroy
   has_many :reports, dependent: :destroy
 
   validates :title, uniqueness: true, presence: true
   validates :country, presence: true
-
-  scope :active, -> { where(archive: 'false') }
 
   def default_help_request_kind
     help_request_kinds.find_by(default: true)
