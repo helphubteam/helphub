adc6324c9c4ba507797c9dcee3587e61d9c7051e
Илья Пономарев <ilya.ponomarev@balance-pl.ru>
2020-10-24 23:12:42 +0800

    allow host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.hosts.clear
   # Code is not reloaded between requests.
   config.cache_classes = true
 
