d5d294f06a09ebad562eaf1bf18d254416f5e9fe
Илья Пономарев <ilya.ponomarev@balance-pl.ru>
2020-11-04 21:41:27 +0800

    fix review

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,5 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.hosts << 'staging.helphub.ru'
   # Code is not reloaded between requests.
   config.cache_classes = true
 
