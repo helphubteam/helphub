8bd3121ef8e7b0a85e91bd7bbe079263abbbffed
Alexey Kirkov <kirkov.alexey@ds.agency>
2020-12-05 22:16:47 +0400

    Fix minor bugs

--- start.sh
+++ start.sh
@@ -11,11 +11,11 @@ rm -f tmp/pids/server.pid
 # bundle install
 # yarn --check-files
 
-# if [ "$RAILS_ENV" = 'production' ]
-# then
-#   bundle exec rake db:create db:migrate assets:precompile
-# else
-#   bundle exec rake db:create db:migrate db:seed
-# fi
+if [ "$RAILS_ENV" = 'production' ]
+then
+  bundle exec rails db:create db:migrate assets:precompile
+else
+  # bundle exec rails db:create db:migrate db:seed
+fi
 
 bundle exec rails s -p 3000 -b 0.0.0.0
