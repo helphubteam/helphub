aaf6c14646e97aca9466adb0a990c5b7961ed5e0
Alexey Kirkov <kirkov.alexey@ds.agency>
2020-12-06 00:45:18 +0400

    Fix config

--- start.sh
+++ start.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 
-until psql -h postgres -U postgres -c '\l'; do
-  echo "Postgres is unavailable - sleeping for 5 seconds"
-  sleep 5
-done
-echo "Postgres is up - executing command"
+# until psql -h postgres -U postgres -c '\l'; do
+#   echo "Postgres is unavailable - sleeping for 5 seconds"
+#   sleep 5
+# done
+# echo "Postgres is up - executing command"
 
 rm -f tmp/pids/server.pid
 # bundle install
@@ -15,7 +15,7 @@ if [ "$RAILS_ENV" = 'production' ]
 then
   bundle exec rails db:create db:migrate assets:precompile
 else
-  # bundle exec rails db:create db:migrate db:seed
+  bundle exec rails db:create db:migrate db:seed
 fi
 
 bundle exec rails s -p 3000 -b 0.0.0.0
--- start_sidekiq.sh
+++ start_sidekiq.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 
-until psql -h postgres -U postgres -c '\l'; do
-  echo "Postgres is unavailable - sleeping for 5 seconds"
-  sleep 5
-done
-echo "Postgres is up - executing command"
+# until psql -h postgres -U postgres -c '\l'; do
+#   echo "Postgres is unavailable - sleeping for 5 seconds"
+#   sleep 5
+# done
+# echo "Postgres is up - executing command"
 
 # bundle install
 bundle exec sidekiq -C config/sidekiq.yml
