be88f0e7e134f9a2ee4693e7d52131d4b005a49a
Alexey Kirkov <kirkov.alexey@ds.agency>
2021-04-09 22:16:12 +0400

    Cleanup code

--- start.sh
+++ start.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 set -e
 
-# until psql -h postgres -U postgres -c '\l'; do
-#   echo "Postgres is unavailable - sleeping for 5 seconds"
-#   sleep 5
-# done
-# echo "Postgres is up - executing command"
-
 rm -f tmp/pids/server.pid
-# bundle install
-# yarn --check-files
 
 if [ "$RAILS_ENV" = 'production' ] || [ "$RAILS_ENV" = 'staging' ]
 then
