e5cae502b0654389e03f1dec7814c30ff1155721
Alexey Kuznetsov <m2lxkuz@gmail.com>
2021-04-16 03:22:02 +0500

    Upgrade registration form

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,13 +1,22 @@
 class RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
 
+  def new
+    @organizations = available_organizations
+    super
+  end
+
   protected
+
+  def available_organizations
+    Organization.active.where(test: false)
+  end
 
   def sign_up_params
     super.merge(volunteer: true, status: :on_check)
   end
 
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:organization_id])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:organization_id, :name, :surname, :phone])
   end
 end
