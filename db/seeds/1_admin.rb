de19b748f651e5c927f54131d0e0796358049f64
Valeriy <sten.vl2010@gmail.com>
2021-05-01 22:36:55 +0500

    Add recaptcha

--- db/seeds/1_admin.rb
+++ db/seeds/1_admin.rb
@@ -3,6 +3,7 @@ unless User.any?
     roles: { 'admin' => true },
     email: 'admin@covihelp.ru',
     password: 'covihelp19',
-    password_confirmation: 'covihelp19'
+    password_confirmation: 'covihelp19',
+    confirmed_at: '2021-01-01 00:00:00'
   )
 end
