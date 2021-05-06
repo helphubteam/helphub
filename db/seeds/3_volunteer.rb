ec92976c4cb3e0b279e4a242cad60aee787fbdff
yuraefremov <yuraefremov18228@gmail.com>
2021-05-06 12:05:24 +0600

    Fix the seed volunteer in the project

--- db/seeds/3_volunteer.rb
+++ db/seeds/3_volunteer.rb
@@ -1,6 +1,9 @@
 unless User.volunteers.any?
   User.create!(
     roles: { 'volunteer' => true },
+    name: 'Волонтир',
+    surname: 'Волонтиров',
+    phone: '+76666666666',
     email: 'volunteer@covihelp.ru',
     password: 'covihelp19',
     password_confirmation: 'covihelp19',
