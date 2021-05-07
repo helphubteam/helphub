c2d3a169ee1c8a842263b12274dfa27c4b71826c
Alexey Kirkov <kirkov.alexey@ds.agency>
2021-05-07 08:09:22 +0400

    Added sentry for frontend

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,7 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-
+require('./sentry')
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require('cocoon-js')
