7df833d580d87afb14c8aee9f313a50502458afd
yuraefremov <yuraefremov18228@gmail.com>
2021-06-22 17:19:45 +0600

    Message about a new volunteer is sent to the moderator

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,4 +7,11 @@ class UserMailer < ApplicationMailer
     @organization = Organization.find(organization_id)
     mail(to: @user.email, subject: "Вы добавлены в фонд #{@organization.title}")
   end
+
+  def new_volunteer_confirmation(moderator_id:, user_id:, organization_id:)
+    @user = User.find(user_id)
+    @moderator = User.find(moderator_id)
+    @organization = Organization.find(organization_id)
+    mail(to: @moderator.email, subject: "Новый волонтер в #{@organization.title}")
+  end
 end
