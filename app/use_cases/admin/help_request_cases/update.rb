7487a83ebbb44cbfc87d815e33e5a47e1af9b8c5
Alexey Kuznetsov <m2lxkuz@gmail.com>
2021-06-30 11:27:52 +0300

    Fix update request activity logging

--- app/use_cases/admin/help_request_cases/update.rb
+++ app/use_cases/admin/help_request_cases/update.rb
@@ -4,25 +4,36 @@ module Admin
       def call
         old_volunteer = help_request.volunteer
         if help_request.update(permitted_params)
+          write_update_log(help_request, permitted_params)
           handle_blocking!
           handle_volunteer_assignments!(old_volunteer)
           notify_on_update!(help_request, help_request.volunteer, current_user) if old_volunteer && help_request.volunteer == old_volunteer
-          write_update_log(help_request, permitted_params)
           return true
         end
 
         false
       end
     
-      private 
+      private
+
+      LOG_EXCLUDE_FIELDS = [
+          :created_at,
+          :updated_at,
+          :lonlat,
+          :lonlat_with_salt,
+          :volunteer_id,
+          :state,
+          :activated_at
+      ].freeze
 
       def write_update_log(help_request, permitted_params)
-        comment = [] 
+        comment = []
+
         help_request.previous_changes.each do |field, (old_value, new_value)|
-          next if [:created_at, :updated_at, :lonlat, :lonlat_with_salt].include? field.to_sym
+          next if LOG_EXCLUDE_FIELDS.include? field.to_sym
           comment << I18n.t("activerecord.attributes.help_request.#{field}") + ": #{old_value} -> #{new_value}" 
         end
-        write_moderator_log(:updated, comment.join(";     "))
+        write_moderator_log(:updated, comment.join(";     ")) if comment.any?
       end
     end
   end
