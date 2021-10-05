4f78c8d889d64cb3cdcc4d3c577e78084824e8e3
Alexey Kuznetsov <alexey.kuznetsov@idt.net>
2021-10-06 01:10:21 +0500

    Handle review comments regarding roles update

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,23 +1,67 @@
 class UserPolicy < ApplicationPolicy
   def create?
-    user.admin? || (user.moderator? && !set_user.admin?)
+    user.admin? || (
+      (user.organization_id == set_user.organization_id) &&
+      user.moderator? && !set_user.admin?
+    )
   end
 
   def update?
     return true if user.admin? || (
-      user.moderator? && !set_user.admin? 
-    ) || (
-      user.content_manager? && set_user == user
+      (user.organization_id == set_user.organization_id) &&
+      (
+        (user.moderator? && !set_user.admin?) || 
+        (user.content_manager? && set_user == user)
+      )
     )
     false
   end
 
   def destroy?
-    user.admin? || user.moderator?
+    user.admin? || (
+      (user.organization_id == set_user.organization_id) &&
+      user.moderator? && !set_user.admin?
+    )
   end
 
   def approve?
-    user.admin? || (user.moderator? && !set_user.admin?)
+    user.admin? || (
+      (user.organization_id == set_user.organization_id) &&
+      user.moderator? && !set_user.admin?
+    )
+  end
+
+  def update_admin_role?
+    user.admin?
+  end
+
+  def update_moderator_role?
+    user.admin? || (
+      (user.organization_id == set_user.organization_id) &&
+      user.moderator?
+    )
+  end
+
+  def update_volunteer_role?
+    user.admin? || (
+      (user.organization_id == set_user.organization_id) &&
+      (
+        user.moderator? || (
+          set_user == user && user.content_manager?
+        )
+      )
+    )
+  end
+
+  def update_content_manager_role?
+    user.admin? || (
+      (user.organization_id == set_user.organization_id) &&
+      (
+        user.moderator? || (
+          set_user == user && user.content_manager?
+        )
+      )
+    )
   end
 
   class Scope < Scope
